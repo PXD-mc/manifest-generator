0e2b50223527bd62e2f64a08f92698124f9c3f4f
PXD <79040730+PXD-mc@users.noreply.github.com>
2025-01-22 21:58:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import uuid.uuid4, json.dump
+import uuid, json
 def manif():
   try:
     print(r'use \n','to seprate lines.\n',"\nMinecraft text colour codes:\n§0 = Black       §8 = Gray\n§1 = Blue        §9 = Light Blue\n§2 = Green       §A = Light Green\n§3 = Aqua        §B = Light Aqua\n§4 = Red         §C = Light Red\n§5 = Purple      §D = Light Purple\n§6 = Yellow      §E = Light Yellow\n§7 = White       §F = Bright White\n§l = Bold        §o = Italic\n§k = Changing letter thing (?)\n\n")
@@ -6,3 +6,4 @@ def manif():
       json.dump({"format_version":1,"header":{"description":input('Pack Description - '),"min_engine_version":[1,10,0],"name":input('Pack Name - '),"uuid":str(uuid.uuid4()),"version":[0,0,1]},"modules":[{"description":" ","type":"resources","uuid":str(uuid.uuid4()),"version":[0,0,1]}]},f,indent=3)
     print('File successfully created!')
   except Exception as exception : print(f'Script ran into an error!\n{exception}')
+manif()
